b5f4f884d832d39475b298b3d438c4f8e2b29ae4
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2022-10-06 19:06:32 -0400

    adds red error borders, creates conditionals CORRECTLY for javascript

--- card-payment.js
+++ card-payment.js
@@ -1,24 +1,30 @@
 const month = document.querySelector("#month");
 const year = document.querySelector("#year");
 const cvc = document.querySelector("#CVC");
-const errorMessage = document.querySelector("#error");
+const errorMessage = document.querySelector("error-border");
 const confirmButton = document.querySelector("#confirm-button");
+const confirmationMessage = document.querySelector("#confirmation-message");
 
 // to-do:
 // 2) if all info entered correctly, confirm button should replace 16 digit number with # entered in cardnumber blank and cvc
 // 3) confirm button should also take you to pop up that says thank you for submitting
 
-// 1. if everything is typed in wrong or right
-confirmButton.addEventListener("click", () => {
+function doTheThing(something) {
   if (month.value === "" || month.value == null) {
-    // if month blank //
-    alert("Can't be blank"); // display message can't be blank
-  } else if (year.value === "" || year.value == null) {
-    // if year blank //
-    alert("Can't be blank"); // red alert can't be blank //
-  } else if (cvc.value === "" || cvc.value == null) {
-    alert("can't be blank");
-  } else e.preventDefault(); // then submit form = show confirmation message "thank you we've added your card details continue button LEAVE THIS //
-});
+    // add error-border class to input field
+    month.classList.add("error-border");
+  }
 
-// 2. when typed correctly, submit button displays thank you screen
+  if (year.value === "" || year.value == null) {
+    year.classList.add("error-border"); // red alert can't be blank //
+  }
+
+  if (cvc.value === "" || cvc.value == null) {
+    cvc.classList.add("error-border");
+  } // then submit form = show confirmation message "thank you we've added your card details continue button LEAVE THIS //
+
+  something.preventDefault();
+}
+
+// 1. if everything is typed in wrong or right
+confirmButton.addEventListener("click", doTheThing);
