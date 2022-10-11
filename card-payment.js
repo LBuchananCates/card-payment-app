3264b8c30764574531482b29e1424d925f0b9c1a
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2022-10-11 12:52:48 -0400

    input fields turn red when blank

--- card-payment.js
+++ card-payment.js
@@ -4,27 +4,90 @@ const cvc = document.querySelector("#CVC");
 const errorMessage = document.querySelector("error-border");
 const confirmButton = document.querySelector("#confirm-button");
 const confirmationMessage = document.querySelector("#confirmation-message");
+const showError = document.querySelector("#error");
 
 // to-do:
 // 2) if all info entered correctly, confirm button should replace 16 digit number with # entered in cardnumber blank and cvc
 // 3) confirm button should also take you to pop up that says thank you for submitting
 
-function doTheThing(something) {
+function showErrorBorder(submitCardInfo) {
   if (month.value === "" || month.value == null) {
-    // add error-border class to input field
     month.classList.add("error-border");
+    // month.classList.add("error-message");
   }
-
   if (year.value === "" || year.value == null) {
-    year.classList.add("error-border"); // red alert can't be blank //
+    year.classList.add("error-border");
+    // year.classList.add("error-message");
   }
-
   if (cvc.value === "" || cvc.value == null) {
     cvc.classList.add("error-border");
-  } // then submit form = show confirmation message "thank you we've added your card details continue button LEAVE THIS //
-
-  something.preventDefault();
+    // cvc.classList.add("error-message");
+  }
+  submitCardInfo.preventDefault();
 }
+confirmButton.addEventListener("click", showErrorBorder);
 
-// 1. if everything is typed in wrong or right
-confirmButton.addEventListener("click", doTheThing);
+const checkMonth = () => {
+  let valid = false;
+  if (!isRequired(month)) {
+    showError(month, "Cannot be blank.");
+  } else {
+    showSuccess(month);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkYear = () => {
+  let valid = false;
+  if (!isRequired(year)) {
+    showError(year, "Cannot be blank.");
+  } else {
+    showSuccess(year);
+    valid = true;
+  }
+  return valid;
+};
+
+const checkCVC = () => {
+  let valid = false;
+  if (!isRequired(cvc)) {
+    showError(cvc, "Cannot be blank.");
+  } else {
+    showSuccess(cvc);
+    valid = true;
+  }
+  return valid;
+};
+
+form.addEventListener("submit", function (e) {
+  // prevent the form from submitting
+  e.preventDefault();
+
+  // validate forms
+  let isMonthValid = checkMonth(),
+    isYearValid = checkYear(),
+    isCVCValid = checkCVC();
+
+  let isFormValid = isMonthValid && isYearValid && isCVCValid;
+
+  // submit to the server if the form is valid
+  if (isFormValid) {
+  }
+});
+// function submitForm() {
+//   if ((month.value = true)) {
+//     submitCardInfo.preventDefault();
+//   }
+//   if ((year.value = true)) {
+//     submitCardInfo.preventDefault();
+//   }
+//   if ((cvc.value = true)) {
+//     submitCardInfo.preventDefault();
+//   }
+// }
+// confirmButton.addEventListener("click", confirmationMessage);
+
+// if all blanks are filled in, submit form and display confirmation messages
+// then submit form = show #confirmation-message "thank you we've added your card details continue
+// button LEAVE THIS and MATCH input card-number with that of card number content of div #fake-number
